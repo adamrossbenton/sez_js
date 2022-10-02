b32a052b4a0f43950c988906f8afdb9ceea51dca
adamrossbenton <adamrossbenton@gmail.com>
2022-10-02 13:50:04 -0700

    added different difficulties, setting up logic for difficulty selection

--- src/Main.js
+++ src/Main.js
@@ -9,6 +9,46 @@ const Main = props => {
         </div>
     }
 
+    const medBoard = () => {
+        return <div className="board" id="med-board">
+            <div className="game-piece" id="med-red">med red</div>
+            <div className="game-piece" id="med-blue">med blue</div>
+            <div className="game-piece" id="med-yellow">med yellow</div>
+            <div className="game-piece" id="med-green">med green</div>
+        </div>
+    }
+
+    const hardBoard = () => {
+        return <div className="board" id="hard-board">
+            <div className="game-piece" id="med-red">med red</div>
+            <div className="game-piece" id="med-blue">med blue</div>
+            <div className="game-piece" id="med-yellow">med yellow</div>
+            <div className="game-piece" id="med-green">med green</div>
+            <div className="game-piece" id="med-orange">med orange</div>
+            <div className="game-piece" id="med-purple">med purple</div>
+        </div>
+    }
+
+    const diffSelect = () => {
+        
+    }
+
+    const chosenBoard = diff => {
+        const diffInput = diff.toLowerCase()
+        switch (diffInput) {
+            case "easy":
+                easyBoard()
+                break;
+            case "medium":
+                medBoard()
+                break;
+            case "hard":
+                hardBoard()
+                break;
+            default:
+        }
+    }
+
     return <div id="main-section">
         <div className="container" id="board-and-title">
             <h3>Click The Colors In Order</h3>
