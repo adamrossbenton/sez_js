916f6f09e6b4d81748d71a662f4681f0c4323712
adamrossbenton <adamrossbenton@gmail.com>
2022-10-02 14:49:12 -0700

    cool I think I got my routes working

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,22 +1,25 @@
 import React from "react";
+import {Outlet, Link} from "react-router-dom"
 
 
 const Main = props => {
 
     const diffSelect = () => {
         return <div className="container" id="diff-select">
+            <Outlet />            
             <h3>Choose Difficulty</h3>
-            <div className="container" id="diff-choices">
-                <form className="diff-choice">
-                    <button type="submit"></button>
-                </form>
+            <div className="container" id="diffs">
+                <Link to={"/easy"}>EASY</Link>
+                <Link to={"/medium"}>MEDIUM</Link>
+                <Link to={"/hard"}>HARD</Link>
             </div>
+            <Link to={"/"}>RESET</Link>
         </div>
     }
 
     return <div id="main-section">
         <div className="container" id="board-and-title">
-            <h3>Click The Colors In Order</h3>
+            {diffSelect()}
         </div>
     </div>
 }
