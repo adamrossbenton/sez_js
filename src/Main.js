a6c4cac39a9781a627f6f6620f0f3abd5257d328
adamrossbenton <adamrossbenton@gmail.com>
2022-10-02 14:03:22 -0700

    fixing auto difficulty select hard

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Main = props => {
+
+    // Difficulty state
+    const [difficulty, setDifficulty] = useState()
+
+    console.log(difficulty)
 
     const easyBoard = () => {
         return <div className="board" id="easy-board">
@@ -20,22 +25,28 @@ const Main = props => {
 
     const hardBoard = () => {
         return <div className="board" id="hard-board">
-            <div className="game-piece" id="med-red">med red</div>
-            <div className="game-piece" id="med-blue">med blue</div>
-            <div className="game-piece" id="med-yellow">med yellow</div>
-            <div className="game-piece" id="med-green">med green</div>
-            <div className="game-piece" id="med-orange">med orange</div>
-            <div className="game-piece" id="med-purple">med purple</div>
+            <div className="game-piece" id="med-red">hard red</div>
+            <div className="game-piece" id="med-blue">hard blue</div>
+            <div className="game-piece" id="med-yellow">hard yellow</div>
+            <div className="game-piece" id="med-green">hard green</div>
+            <div className="game-piece" id="med-orange">hard orange</div>
+            <div className="game-piece" id="med-purple">hard purple</div>
         </div>
     }
 
     const diffSelect = () => {
-        
+        return <div className="container" id="diff-select">
+            <h3>Choose Difficulty</h3>
+            <div className="container" id="diff-choices">
+                <button>EASY</button>
+                <button>MEDIUM</button>
+                <button>HARD</button>
+            </div>
+        </div>
     }
 
     const chosenBoard = diff => {
-        const diffInput = diff.toLowerCase()
-        switch (diffInput) {
+        switch (difficulty) {
             case "easy":
                 easyBoard()
                 break;
@@ -52,6 +63,7 @@ const Main = props => {
     return <div id="main-section">
         <div className="container" id="board-and-title">
             <h3>Click The Colors In Order</h3>
+            {difficulty? chosenBoard(difficulty) : diffSelect()}
         </div>
     </div>
 }
